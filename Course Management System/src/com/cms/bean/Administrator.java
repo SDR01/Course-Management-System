d2720f9053b35b48ee88cf1387b9ff3d31e64b2d
R Sai Datta Reddy <saidattareddy007@gmail.com>
2022-09-29 19:51:07 +0530

    Administrator Bean Class

--- Course Management System/src/com/cms/bean/Administrator.java
+++ Course Management System/src/com/cms/bean/Administrator.java
@@ -2,7 +2,16 @@ package com.cms.bean;
 
 public class Administrator {
 	
+	String username = "Admin";
+	String password = "Alpha01";
 	
-	
+	public Administrator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Administrator(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
 
 }
